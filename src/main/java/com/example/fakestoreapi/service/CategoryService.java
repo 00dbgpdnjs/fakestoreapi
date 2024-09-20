c18574c8b46a425ce404dd6c4e3f67cb729a24f9
00dbgpdnjs <81615868+00dbgpdnjs@users.noreply.github.com>
2024-09-20 22:04:49 +0900

    Add getAllCategories

--- src/main/java/com/example/fakestoreapi/service/CategoryService.java
+++ src/main/java/com/example/fakestoreapi/service/CategoryService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
@@ -14,5 +16,10 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public Category getCategory(Long categoryId) {
         return categoryRepository.findById(categoryId).orElseThrow();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
     }
 }
